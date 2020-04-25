76206d51c6805f00f1c36b7c0139d2590838a524
Roopa <roopaworld@yahoo.com>
2020-04-25 09:55:11 +0100

    adding to my branch

---
+++ src/test/java/org/example/Multiplication.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Multiplication {
+    public static void main(String[] args){
+
+        int a=10;
+        int b=12;
+
+        System.out.println(a*b);
+    }
+}
