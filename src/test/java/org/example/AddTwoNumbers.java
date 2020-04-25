22a751c690cdea1924a21165e5ea95bc76a5f881
Roopa <roopaworld@yahoo.com>
2020-04-25 09:37:03 +0100

    second commit

--- src/test/java/org/example/AddTwoNumbers.java
+++ src/test/java/org/example/AddTwoNumbers.java
@@ -3,13 +3,17 @@ package org.example;
 public class AddTwoNumbers {
    static int a;
     static int b;
+    static int c;
+
 
     public static void main(String[] args){
+        int solution;
         a=5;
         b=6;
 
         System.out.println("Hello sum of two numbers is :"+a+b);
-
+        solution = (a+b)/c;
+        System.out.println(solution);
     }
 
 }
